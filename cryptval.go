bf2bbe497fb060f55ca01a108eeaddc2b580775c
Bradley Falzon <brad@teambrad.net>
2017-01-02 20:36:09 +1030

    EncryptBytes, docs

--- cryptval.go
+++ cryptval.go
@@ -10,19 +10,25 @@ import (
 	"io"
 )
 
+// A Cipher encrypts plaintext or decrypts ciphertext.
 type Cipher interface {
-	Encrypt([]byte) ([]byte, error)
-	Decrypt([]byte) ([]byte, error)
+	Encrypt(plaintext []byte) (ciphertext []byte, err error)
+	Decrypt(ciphertext []byte) (plaintext []byte, err error)
 }
 
+// GCM is a cipher using AES in Galois/Counter Mode.
 type GCM struct {
 	key []byte
 }
 
-func NewGCM(key []byte) Cipher {
+// NewGCM256 returns a Cipher using AES-256 in Galois/Counter Mode.
+func NewGCM256(key [32]byte) Cipher {
 	return GCM{key: key[:]}
 }
 
+// Encrypt implements the Cipher interface.
+//
+// See:
 // https://github.com/gtank/cryptopasta/blob/bc3a108a5776376aa811eea34b93383837994340/encrypt.go#L37-L55
 func (c GCM) Encrypt(plaintext []byte) (ciphertext []byte, err error) {
 	block, err := aes.NewCipher(c.key)
@@ -44,6 +50,9 @@ func (c GCM) Encrypt(plaintext []byte) (ciphertext []byte, err error) {
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
+// Decrypt implements the Cipher interface.
+//
+// See:
 // https://github.com/gtank/cryptopasta/blob/bc3a108a5776376aa811eea34b93383837994340/encrypt.go#L60-L80
 func (c GCM) Decrypt(ciphertext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(c.key)
@@ -63,19 +72,29 @@ func (c GCM) Decrypt(ciphertext []byte) ([]byte, error) {
 	return gcm.Open(nil, ciphertext[:gcm.NonceSize()], ciphertext[gcm.NonceSize():], nil)
 }
 
-type Bytes struct {
-	cipher Cipher
-	Bytes  []byte
+// A CryptVal is used to encrypt values for storage in a database, and decrypts
+// them, storing the plaintext in Plaintext.
+type CryptVal struct {
+	cipher    Cipher
+	Plaintext []byte
 }
 
-func NewBytes(cipher Cipher) *Bytes {
-	return &Bytes{cipher: cipher}
+// New returns a CryptVal with the chosen cipher.
+func New(cipher Cipher) *CryptVal {
+	return &CryptVal{cipher: cipher}
+}
+
+// EncryptBytes sets plaintext to be encrypted. Returns itself to support fluent
+// syntax.
+func (s *CryptVal) EncryptBytes(plaintext []byte) *CryptVal {
+	s.Plaintext = plaintext
+	return s
 }
 
 // Value implements the driver Valuer interface by encrypting s.Bytes and
 // returning the ciphertext.
-func (s Bytes) Value() (driver.Value, error) {
-	ciphertext, err := s.cipher.Encrypt(s.Bytes)
+func (s CryptVal) Value() (driver.Value, error) {
+	ciphertext, err := s.cipher.Encrypt(s.Plaintext)
 	if err != nil {
 		return nil, err
 	}
@@ -86,13 +105,13 @@ func (s Bytes) Value() (driver.Value, error) {
 
 // Scan implements the Scanner interface by decrypting value and storing the
 // result in s.Bytes.
-func (s *Bytes) Scan(value interface{}) error {
+func (s *CryptVal) Scan(value interface{}) error {
 	ciphertext := value.([]byte)
 	buf := make([]byte, len(ciphertext))
 	n, err := base64.StdEncoding.Decode(buf, ciphertext)
 	if err != nil {
 		return err
 	}
-	s.Bytes, err = s.cipher.Decrypt(buf[:n])
+	s.Plaintext, err = s.cipher.Decrypt(buf[:n])
 	return err
 }
--- cryptval_test.go
+++ cryptval_test.go
@@ -30,24 +30,23 @@ func TestCryptVal(t *testing.T) {
 		t.Fatal("unexpected error: ", err)
 	}
 
-	key := []byte{0x4f, 0x25, 0xcc, 0xf0, 0xcb, 0x5d, 0xc6, 0x7a, 0x26, 0x1f, 0x13, 0xc4, 0x72, 0x9d, 0x54, 0xc9, 0x9a, 0x9e, 0xfd, 0xf1, 0x6a, 0xe9, 0x45, 0x7f, 0x2e, 0x33, 0xfe, 0xca, 0x80, 0x71, 0x6d, 0x79}
-	encrypt := cryptval.NewBytes(cryptval.NewGCM(key))
-	encrypt.Bytes = []byte("top-secret")
+	key := [32]byte{1}
+	plaintext := []byte("some-secret")
+	encrypt := cryptval.New(cryptval.NewGCM256(key)).EncryptBytes(plaintext)
 
 	_, err = db.Exec("INSERT INTO cv (name) VALUES (?)", encrypt)
 	if err != nil {
 		t.Fatal("unexpected error: ", err)
 	}
 
-	decrypt := cryptval.NewBytes(cryptval.NewGCM(key))
-
+	decrypt := cryptval.New(cryptval.NewGCM256(key))
 	err = db.QueryRow("SELECT name FROM cv").Scan(decrypt)
 	if err != nil {
 		t.Fatal("unexpected error: ", err)
 	}
 
-	if !reflect.DeepEqual(encrypt.Bytes, decrypt.Bytes) {
-		t.Errorf("Failed to encrypt+decrypt\nwant %q\nhave %q", encrypt.Bytes, decrypt.Bytes)
+	if !reflect.DeepEqual(decrypt.Plaintext, plaintext) {
+		t.Errorf("Failed to encrypt+decrypt\nhave %q\nwant %q", decrypt.Plaintext, plaintext)
 	}
 }
 
@@ -57,28 +56,25 @@ func (mockCipher) Encrypt([]byte) ([]byte, error) { return []byte{0, 0, 1}, nil 
 func (mockCipher) Decrypt([]byte) ([]byte, error) { return []byte{0, 0, 2}, nil }
 
 func TestBytesValue(t *testing.T) {
-	encrypt := cryptval.NewBytes(mockCipher{})
-	encrypt.Bytes = []byte("top-secret")
+	cv := cryptval.New(mockCipher{}).EncryptBytes([]byte("top-secret"))
 
-	want := []byte("AAAB")
-	have, err := encrypt.Value()
+	have, err := cv.Value()
 	if err != nil {
-		t.Fatal("unexpected error: ", err)
+		t.Fatal("unexpected error from value: ", err)
 	}
-	if !reflect.DeepEqual(have, want) {
+	if want := []byte("AAAB"); !reflect.DeepEqual(have, want) {
 		t.Errorf("Failed to encrypt\nhave %q\nwant %q", have, want)
 	}
 }
 
 func TestBytesScan(t *testing.T) {
-	encrypt := cryptval.NewBytes(mockCipher{})
+	cv := cryptval.New(mockCipher{})
 
-	want := []byte{0, 0, 2}
-	err := encrypt.Scan([]byte("AAAC"))
+	err := cv.Scan([]byte("AAAC"))
 	if err != nil {
-		t.Fatal("unexpected error: ", err)
+		t.Fatal("unexpected error from scan: ", err)
 	}
-	if !reflect.DeepEqual(encrypt.Bytes, want) {
-		t.Errorf("Failed to encrypt\nhave %q\nwant %q", encrypt.Bytes, want)
+	if want := []byte{0, 0, 2}; !reflect.DeepEqual(cv.Plaintext, want) {
+		t.Errorf("Failed to decrypt\nhave %q\nwant %q", cv.Plaintext, want)
 	}
 }
